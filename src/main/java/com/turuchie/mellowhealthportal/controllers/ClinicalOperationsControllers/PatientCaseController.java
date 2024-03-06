fbd09871b0e9c69cbc13e87f82844364aa8a8ed7
TuruchieNetworks <TuruchieNetworks@outlook.com>
2024-03-06 18:58:21 -0500

    Front End CSS clean up for patientCase

--- src/main/java/com/turuchie/mellowhealthportal/controllers/ClinicalOperationsControllers/PatientCaseController.java
+++ src/main/java/com/turuchie/mellowhealthportal/controllers/ClinicalOperationsControllers/PatientCaseController.java
@@ -5,7 +5,6 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
